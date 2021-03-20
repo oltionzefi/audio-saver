e758f5df3915be91b439c4006f03aa429b061311
Oltion Zefi <oltionzefi@gmail.com>
2021-03-21 00:08:55 +0100

    Encoding in JSON the response; Use CORS; Move defer after error check

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
 	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/martini-contrib/cors v0.0.0-20141016003011-553b9208d353 // indirect
+	github.com/martini-contrib/encoder v0.0.0-20150404114951-f07f7dc73cdc // indirect
 )
--- main.go
+++ main.go
@@ -8,11 +8,19 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 
 	"github.com/go-martini/martini"
 	"github.com/joho/godotenv"
+	"github.com/martini-contrib/cors"
+	"github.com/martini-contrib/encoder"
 )
+
+type Response struct {
+	Message string `json:"login"`
+	Code    string `json:"url"`
+}
 
 func main() {
 	m := martini.Classic()
@@ -25,6 +33,29 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	m.Use(func(c martini.Context, w http.ResponseWriter, r *http.Request) {
+		// Use indentations. &pretty=1
+		pretty, _ := strconv.ParseBool(r.FormValue("pretty"))
+		// Use null instead of empty object for json &null=1
+		null, _ := strconv.ParseBool(r.FormValue("null"))
+		// Some content negotiation
+		switch r.Header.Get("Content-Type") {
+		case "application/xml":
+			c.MapTo(encoder.XmlEncoder{PrettyPrint: pretty}, (*encoder.Encoder)(nil))
+			w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+		default:
+			c.MapTo(encoder.JsonEncoder{PrettyPrint: pretty, PrintNull: null}, (*encoder.Encoder)(nil))
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		}
+	})
+
+	m.Use(cors.Allow(&cors.Options{
+		AllowOrigins:  []string{"http://localhost*", "http://127.0.0.1*"},
+		AllowMethods:  []string{"PUT", "PATCH", "POST", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "X-API-KEY", "Content-Type", "Accept", "Referer", "User-Agent"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
+
 	m.Use(func(res http.ResponseWriter, req *http.Request) {
 		apiKey := envs["API_KEY"]
 
@@ -33,20 +64,20 @@ func main() {
 		}
 	})
 
-	m.Get("/", func(res http.ResponseWriter, req *http.Request) {
-		res.WriteHeader(http.StatusOK)
-		res.Write([]byte("Hello World!"))
+	m.Get("/", func(enc encoder.Encoder) (int, []byte) {
+		result := Response{"Hello World!", "200"}
+		return http.StatusOK, encoder.Must(enc.Encode(result))
 	})
 
-	m.Get("/save/:file", func(params martini.Params, res http.ResponseWriter, req *http.Request) {
+	m.Post("/save/:file", func(enc encoder.Encoder, params martini.Params, res http.ResponseWriter, req *http.Request) (int, []byte) {
 		code := UploadSound(req, params)
 		if code != "200" {
-			res.WriteHeader(http.StatusServiceUnavailable)
-			res.Write([]byte("Error happened during save!"))
+			result := Response{"Error happened during save!", code}
+			return http.StatusServiceUnavailable, encoder.Must(enc.Encode(result))
 		}
 
-		res.WriteHeader(http.StatusOK)
-		res.Write([]byte("Saved!"))
+		result := Response{"Saved!", code}
+		return http.StatusOK, encoder.Must(enc.Encode(result))
 	})
 
 	port := "3000"
@@ -73,20 +104,20 @@ func main() {
 func UploadSound(r *http.Request, params martini.Params) string {
 	file, _, err := r.FormFile("audio")
 
-	defer file.Close()
-
 	if err != nil {
 		return "500"
 	}
+
+	defer file.Close()
 
 	filename := fmt.Sprintf("upload/%s.wav", params["file"])
 	out, err := os.Create(filename)
 
-	defer out.Close()
-
 	if err != nil {
 		return "500"
 	}
+
+	defer out.Close()
 
 	_, err = io.Copy(out, file)
 	if err != nil {
